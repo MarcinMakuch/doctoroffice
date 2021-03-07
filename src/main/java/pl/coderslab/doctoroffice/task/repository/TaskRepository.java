b4dc93a98d6630c9b9dacac2ba939d0dcfa3ff68
MarcinMakuch <makuchmarcin0@gmail.com>
2021-03-07 18:50:28 +0100

    walidacja czasu

--- src/main/java/pl/coderslab/doctoroffice/task/repository/TaskRepository.java
+++ src/main/java/pl/coderslab/doctoroffice/task/repository/TaskRepository.java
@@ -1,6 +1,6 @@
 package pl.coderslab.doctoroffice.task.repository;
 
-import org.apache.tomcat.jni.Local;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -17,11 +17,5 @@ public interface TaskRepository extends JpaRepository<Task, Long> {
 
     @Query("select t from Task t where t.date=?1 and t.time=?2")
     Optional <Task> manualFindDateAndTimeFromTasks(@Param("date") Date date, @Param("time") LocalTime time);
-
-/*    @Query(value = "select * from tasks", nativeQuery = true)
-    Optional <Task> manualFindDateAndTime(Date date, LocalTime time);*/
-
-/*    @Query("select b from Book b where b.rating between :rating1 and :rating2")
-    List<Book> manualFindBooksWithRatingBetween(@Param("rating1") int rating1, @Param("rating2") int rating2);*/
 
 }
